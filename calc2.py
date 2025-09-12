90b467507eae35485f3c32e1c9435baac5318cb6
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-12 11:34:24 -0400

    Update calc2.py

--- calc2.py
+++ calc2.py
@@ -6,37 +6,37 @@ print ("4. division")
 option = input ("Enter your option:")
 
 if option == "addition":
-    print ("What is num1 and num 2?")
-    x = inp
+    x = input ("What is num 1?")
+    y = input ("What is num 2?")
+    x = int(x)
+    y = int(y)
+    z = x + y
+    z = int(z)
+    print (f"The sum of {x} and {y} is {z}")
 elif option =="subtraction":
-    print ("Your grade level is 10")
+    x = input("What is num 1?")
+    y = input ("What is num 2?")
+    x = int(x)
+    y = int(y)
+    z = x - y
+    z = int(z)
+    print (f"The difference of {x} and {y} is {z}")
+
 elif option == "multiplication":
-    print ("Your grade level is 11")
+    x = input("What is num 1?")
+    y = input ("What is num 2?")
+    x = int(x)
+    y = int(y)
+    z = x * y
+    print (f"The product of {x} and {y} is {z}")
+   
 elif option == "division":
-    print ("Your grade level is 12")
+    x = input("What is num 1?")
+    y = input ("What is num 2?")
+    x = int(x)
+    y = int(y)
+    z = x / y
+    print (f"The quotient of {x} and {y} is {z}")
     
 
 
-x = input("What is num 1?")
-y = input ("What is num 2?")
-x = int(x)
-y = int(y)
-z = x - y
-z = int(z)
-print (f"The difference of {x} and {y} is {z}")
-
-x = input("What is num 1?")
-y = input ("What is num 2?")
-x = int(x)
-y = int(y)
-z = x * y
-z = int(z)
-print (f"The product of {x} and {y} is {z}")
-
-x = input("What is num 1?")
-y = input ("What is num 2?")
-x = int(x)
-y = int(y)
-z = x / y
-z = int(z)
-print (f"The quotient of {x} and {y} is {z}")
