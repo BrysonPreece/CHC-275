578fc8da20bf29d2661f749d6768a60ce2f26c70
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-17 10:07:05 -0400

    Update calc3.py

--- calc3.py
+++ calc3.py
@@ -5,39 +5,39 @@ print ("3. multiplication")
 print ("4. division")
 check = False
 while check == False:
-        option = input ("Enter your option 1. addition 2. subtraction 3. multiplication 4. division. Type quit to exit")
-if option == "addition":
-    x = input ("What is num 1?")
-    y = input ("What is num 2?")
-    x = int(x)
-    y = int(y)
-    z = x + y
-    z = int(z)
-    print (f"The sum of {x} and {y} is {z}")
-elif option =="subtraction":
-    x = input("What is num 1?")
-    y = input ("What is num 2?")
-    x = int(x)
-    y = int(y)
-    z = x - y
-    z = int(z)
-    print (f"The difference of {x} and {y} is {z}")
+    option = input ("Enter your option 1. addition 2. subtraction 3. multiplication 4. division. Type quit to exit")
+    if option == "addition":
+        x = input ("What is num 1?")
+        y = input ("What is num 2?")
+        x = int(x)
+        y = int(y)
+        z = x + y
+        z = int(z)
+        print (f"The sum of {x} and {y} is {z}")
+    elif option =="subtraction":
+        x = input("What is num 1?")
+        y = input ("What is num 2?")
+        x = int(x)
+        y = int(y)
+        z = x - y
+        z = int(z)
+        print (f"The difference of {x} and {y} is {z}")
 
-elif option == "multiplication":
-    x = input("What is num 1?")
-    y = input ("What is num 2?")
-    x = int(x)
-    y = int(y)
-    z = x * y
-    print (f"The product of {x} and {y} is {z}")
+    elif option == "multiplication":
+        x = input("What is num 1?")
+        y = input ("What is num 2?")
+        x = int(x)
+        y = int(y)
+        z = x * y
+        print (f"The product of {x} and {y} is {z}")
    
-elif option == "division":
-    x = input("What is num 1?")
-    y = input ("What is num 2?")
-    x = int(x)
-    y = int(y)
-    z = x / y
-    print (f"The quotient of {x} and {y} is {z}")
-elif option == "quit":
-    print ("Goodbye")
-    check = True
+    elif option == "division":
+        x = input("What is num 1?")
+        y = input ("What is num 2?")
+        x = int(x)
+        y = int(y)
+        z = x / y
+        print (f"The quotient of {x} and {y} is {z}")
+    elif option == "quit":
+        print ("Goodbye")
+        check = True
