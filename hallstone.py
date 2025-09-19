8c10e783274a6cd487cc1dd0ed40ecb30df55811
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-19 17:33:30 -0400

    Update hallstone.py

--- hallstone.py
+++ hallstone.py
@@ -3,9 +3,9 @@ option = input ("Enter a number.")
 option = int(option)
 while check == False:
         if option % 2 ==1:
-                option = input (f"{option * 3 + 1}")
+                option = option * 3 + 1
                 print (option)
         elif option % 2 ==0:
-                option = input (f"{option / 2}")
+                option = option / 2
                 print (option)
                 check = True
