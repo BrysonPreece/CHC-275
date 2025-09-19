2fea9238a5743649a73b0a62fbc633af4d316b45
BrysonPreece <PreeceB27@chcstudent.com>
2025-09-19 09:26:31 -0400

    Update hallstone.py

--- hallstone.py
+++ hallstone.py
@@ -1,4 +1,8 @@
 check = False
 while check == False:
-    option = input ("Enter a number.")
-    
+    x = input ("What is num 1?")
+    x = int(x)
+    y = x % 2
+    if x % 2:
+        x % 2 = y
+        
