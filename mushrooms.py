8bfe33c9cd19c8a54cf50588212f9ca6042e230e
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-15 10:14:27 -0400

    Create mushrooms.py

---
+++ mushrooms.py
@@ -0,0 +1,11 @@
+check = False
+option = int(input("What is the size of your mushroom? Type a number"))
+if option < 100:
+    print("Small mushrooms are less than 100 in size")
+elif option > 100 and option < 200:
+    print ("Medium mushrooms are mushrooms between 100 and 200 in size")
+elif option > 200:
+    print("That is a big boy!!! Large mushrooms are greater than or equal to 200 in size")
+else:
+    print ("That is not a size of the mushroom or not a mushroom at all!!! HOW DARE YOU!!!!!!!!!!!")
+    check = True
