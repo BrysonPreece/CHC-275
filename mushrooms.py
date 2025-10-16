b0b962eaaecbd3fdf3fe46f4c991a550388a12c0
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-16 09:01:55 -0400

    Update mushrooms.py

--- mushrooms.py
+++ mushrooms.py
@@ -1,14 +1,20 @@
-check = False
-while check == False:
+option = None
+
+while True:
     option = input("What is the size of your mushroom? Type a number or type 'quit': ").strip().lower()
     if option == "quit":
         print("Exiting the program. Goodbye!")
-    elif option < 100:
+        break
+
+    try:
+        size = int(option)
+    except ValueError:
+        print("That is not a size of the mushroom or not a mushroom at all!!! HOW DARE YOU!!!!!!!!!!!")
+        continue
+
+    if size < 100:
         print("Small mushrooms are less than 100 in size")
-    elif option >= 100 and option < 200:
+    elif 100 <= size < 200:
         print("Medium mushrooms are mushrooms between 100 and 200 in size")
-    elif option > 200:
+    else:
         print("That is a big boy!!! Large mushrooms are greater than or equal to 200 in size")
-else:
-    print("That is not a size of the mushroom or not a mushroom at all!!! HOW DARE YOU!!!!!!!!!!!")
-    check = True
