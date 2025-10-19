cd408846f9f42de61361d33ad92571b2303d4239
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-19 13:35:59 -0400

    commit

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,10 @@
-pdrome = None
+word = None
 
 while True:
-    pdrome = input("Enter a word to see if it is a palindrome or not").strip().lower()
+	word = input("Enter a word to see if it is a palindrome or not (or press Enter to quit): ").strip().lower()
+	if word == "":
+		break
+	if word == word[::-1]:
+		print(f"'{word}' is a palindrome.")
+	else:
+		print(f"'{word}' is not a palindrome.")
