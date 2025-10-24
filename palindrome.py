fdb8086913c3bafd963c1fa2e52de61e17bc8273
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-24 09:30:03 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,6 +2,7 @@ word = None
 
 while True:
 	word = input("Enter a word to see if it is a palindrome or not (or press Enter to quit): ").strip().lower()
+
 	if word == "":
 		break
 	if word == word[::-1]:
