8a155875bf5aa6aab06446c55f410ec60dbb07a2
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-29 13:11:31 -0400

    Update escaperoom.py

--- escaperoom.py
+++ escaperoom.py
@@ -2,15 +2,40 @@ print("Welcome to the Escape Room!!!")
 print("You are traveling in as train traveling to go see your grandparents for Christmas. Suddenly, you get knocked out unexpectedly by a hard object.")
 print("You then fall onto the floor unconscious and you can't remember anything.")
 print("You wake up tied up in a chair in the caboose of the train. You need to get to the front of the train to stop the train and escape.")
+print ("**************************************************************************************************")
+
 check = False
 while check == False:
-    option = input("You look around the room. You see a knife on the floor. Press P to pick up the knife. ").strip().lower().split()
-    option = "".join(option)
+    option = input("You look around the room. You see a knife on the floor. Press P to pick up the knife. ").strip().lower()
     if option == "p":
         print ("**************************************************************************************************")
         print("You picked up the knife to cut the rope. You are now free to move around the room.")
         print("You look around the room. There is a door with a keyhole. You need a key to escape the Caboose.")
         print("There is a painting on the wall, a shelf that is too high to reach, a beanbag, and a safe with a combination to unlock. What do you do? ")
-        print("1. Investigate the safe, 2. Investigate the shelf, 3. Investigate the beanbag, 4. Investigate the painting. TYPE THE NUMBER THAT CORRESPONDS TO THE ACTION")
+        check = True
+check = False
+while check == False:
+    option = input("1. Investigate the safe, 2. Investigate the shelf, 3. Investigate the beanbag, 4. Investigate the painting. TYPE THE NUMBER THAT CORRESPONDS TO THE ACTION ").strip().lower()
+    print("**************************************************************************************************")
+    if option == "1" or option == "1.":
         print ("**************************************************************************************************")
-    check = True
+        print("You investigate the safe. A combination is needed to unlock it.")
+        print ("**************************************************************************************************")
+        check = True
+
+    elif option == "2" or option == "2.":
+        print ("**************************************************************************************************")
+        print ("You investigate the shelf. It looks like something is up there that can be useful. But you can't reach it because you are only 4'1 and not 6'7.")
+        print ("**************************************************************************************************")
+        check = True
+
+    elif option == "3" or option == "3.":
+        print ("**************************************************************************************************")
+        print(" You investigate the beanbag. It looks like you found a hammer. That can be useful for breaking things.")
+        print ("**************************************************************************************************")
+        check = True
+
+    elif option == "4" or option == "4.":
+        print ("**************************************************************************************************")
+        print("There is a painting on the wall. It gives you a chill. It looks like it can be broken with something.")
+        check = True
