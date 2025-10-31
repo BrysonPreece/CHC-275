bb7934a749c7950801e8ea93961f248913bf4eae
BrysonPreece <PreeceB27@chcstudent.com>
2025-10-31 11:44:12 -0400

    Update escaperoom.py

--- escaperoom.py
+++ escaperoom.py
@@ -35,24 +35,14 @@ while check == False:
         print("**************************************************************************************************")
         tool = input("Type HAMMER to use it now, or press Enter to go back: ").strip().lower().split()
         tool = "".join(tool)
-        if tool == "hammer":
+        if tool == "Hammer":
             option = input("Where would you like to use the hammer? 1. Painting, 2. Shelf, 3. Beanbag, 4. Door, 5. Safe. TYPE THE NUMBER THAT CORRESPONDS WITH THE ACTION ").strip().lower()
-            print("**************************************************************************************************")
-            check = False
-            while check == False:
-                if option == "1" or option == "1.":
-                    print("You use the hammer on the painting. It breaks apart and reveals a huge hole on the other side.")
-                    print("You step in the hole and find a ladder.")
-                    print("You look on the wall and see words that say: THERE IS NO ESCAPE!!!")
-                elif option == "2"or "2."or "3"or "3."or "4"or "4."or "5"or "5.":
-                    print("You tried to use the Hammer, but it failed.")
-                    check = True
-                else:
-                    print("That is not an option that you can do.")
-                    check = True
+            if option == "1" or option == "1.":
+                print("You use the hammer on the painting. It breaks apart and reveals a huge hole on the other side.")
+                print("You step in the hole and find a ladder.")
+                print("You look on the wall and see words that say: THERE IS NO ESCAPE!!!")
+            elif option == "2"or "2."or "3"or "3."or "4"or "4."or "5"or "5.":
+                print("You tried to use the Hammer, but it failed.")
+                check = True
     elif option == "4" or option == "4.":
-        print("**************************************************************************************************")
-        print("There is a painting on the wall. It gives you a chill. It looks like it can be broken with something.")
-        print("**************************************************************************************************")
-    else:
-        print("That is not an option that you can do.")
+        print("You Investigate the painting. It shows a picture of an old man. It reminds you of the book that you read in Freshman year of high school. The Old Man and the Sea.")
