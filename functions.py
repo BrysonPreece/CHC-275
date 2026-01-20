fff2c984d686a3f04cc393aee5889a407b7d605a
ferusutajp <prestonmetil@gmail.com>
2026-01-20 10:12:10 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,42 @@
+def foo(): # def- <function name>(): is called the function header
+            #includes the function defination, the function name, and the parameter list
+    print("bar")
+foo() #Always need to end with parentheses. Tab back over to left
+
+def add5(num):
+    #function name = add5
+    #parameter list = num
+    #num is a placeholder
+    num = num + 5
+    print(num)
+add5(8)
+add5(4)
+mynum = 10
+add5(mynum)
+
+def multiply (num1, num2):
+    #function name = multiply
+    #parameter list = num1 and num2
+    
+    product = num1*num2
+    print(product)
+multiply(3,5)
+multiply(4,10)
+multiply(4,"a")
+
+def add(num1, num2):
+    sum = num1+num2
+    print(sum)
+add(2,5)
+add("Hello","World")
+add("World", "Hello")
+
+def studentUpdate(name,age,gpa):
+    print(f"Student Name: {name}, Student Age: {age}, Student gpa: {gpa}")
+studentUpdate("bob", 15,4.0)
+
+def update(num1):
+    num1 = num1 + 10
+x = 5
+update(x)
+print(x)
