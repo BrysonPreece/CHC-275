4ff14051dd7e423314ff89604805e63264140e52
BrysonPreece <PreeceB27@chcstudent.com>
2026-01-29 10:20:14 -0500

    Update getMin.py

--- getMin.py
+++ getMin.py
@@ -1,8 +1,22 @@
-def getMin(list):
-    smallest = list[0]
+def getMin(userlist):
+    min = userlist[0]
     i = 0
-    while i < len(list):
-        if list[i] < smallest:
-            smallest = list[i]
+    while i < len(userlist):
+        if userlist[i] < min:
+            min = userlist[i]
         i = i + 1
-    return smallest
+    return min
+
+first = [1,2,3,4]
+print(first)
+print(f"Minimum of the first list {getMin(first)}")
+
+second = [4,2,1,3]
+print(second)
+print(f"Minimum of the second list {getMin(second)}")
+
+third = [67,69,41,72,94]
+print(third)
+print(f"Minimum of the third list {getMin(third)}")
+
+print("You found the minimum. Great Job!!!")
