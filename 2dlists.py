8b14f7838abee58968ff53fdf973e15beca851ff
ferusutajp <prestonmetil@gmail.com>
2026-02-20 09:30:01 -0500

    Update 2dlists.py

--- 2dlists.py
+++ 2dlists.py
@@ -28,11 +28,33 @@ board = [
     [0,0,0],  
     [0,0,0]    
 ]
+currentplayer = "O"
 print(board)
 def printBoard(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             print("|", end = "")
             print(board[i],[j], end = "|")
-            print()
+        print()
 printBoard(board)
+
+def placepiece(board,currentplayer,row,col):
+    if board[row][col] == 0:
+        board[row][col] = currentplayer
+    else:
+        print("That is not a valid square")
+def switchplayer(currentplayer):
+    if currentplayer == "O":
+        return "X"
+    elif currentplayer == "X":
+        return "O"
+def checkwinner(board,currentplayer):
+    for i in range(len(board)):
+        if board[i][0] == currentplayer and board [i][1] == currentplayer and board[i][2] == currentplayer:
+            print(f"{currentplayer} wins!")
+            return True
+        
+        
+    for j in range(len(board[0])):
+        if board[0][j] == currentplayer and board[1][j] == currentplayer and board[2][j] == currentplayer:
+            print(f"{currentplayer}wins!")
