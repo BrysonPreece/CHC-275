c66ca9068c7d8d900ae7583e94324901ed22f367
ferusutajp <prestonmetil@gmail.com>
2026-02-26 09:29:04 -0500

    commit

--- tictacto.py
+++ tictacto.py
@@ -1,33 +1,14 @@
-grid = [
-    [0,0,0,0],  
-    [0,0,0,0],  
-    [0,0,0,0],      
-]
-print(grid[0][0])
-
-"The syntax is [row],[col]"
-
-grid2 = [
-    [1,2,3],  
-    [4,5,6],  
-    [7,8,9],      
-]
-print(grid2[2][2])
-
-for row in grid2:
-    for num in row:
-        print(num)
-        
-for i in range(len(grid2)):
-    for j in range(len(grid2[0])):
-        print(grid2[i][j])
-        print(f"({i},{j}) = {grid2[i][j]}", end  = " ")
-
 board = [
     [0,0,0],  
     [0,0,0],  
     [0,0,0]    
 ]
+grid = [
+    [0,0,0],  
+    [0,0,0],  
+    [0,0,0]   
+]
+
 currentplayer = "O"
 print(board)
 def printBoard(board):
@@ -41,8 +22,10 @@ printBoard(board)
 def placepiece(board,currentplayer,row,col):
     if board[row][col] == 0:
         board[row][col] = currentplayer
+        return True
     else:
         print("That is not a valid square")
+        return False
 def switchplayer(currentplayer):
     if currentplayer == "O":
         return "X"
@@ -72,3 +55,23 @@ def checkwinner(board,currentplayer):
             if space == 0:
                 return False
     return True
+
+def main():
+    grid = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+curr = "X"
+while checkwinner(grid,curr) == False:
+    curr = switchplayer(curr)
+    check = False
+    while check == False:
+        row = int(input(f"Enter the Row: ").strip())
+        col = int(input(f"Enter the Col: ").strip())
+        if placepiece(grid,curr,row,col) == True:
+            check = True
+
+if __name__ == "__main__":
+    main()
+    
