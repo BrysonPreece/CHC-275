8a28d7085341c1f27777b54fecb182c04e0e775f
BrysonPreece <PreeceB27@chcstudent.com>
2026-03-08 12:56:53 -0400

    commit

--- connectfour.py
+++ connectfour.py
@@ -1,18 +1,17 @@
 """
-Name: Bryson
-Section: 1
-Description: Template for Lab 6. Connect Four Lab
+Name: Bryson Preece
+Section: 01
+Description: Template for Lab 6
 """
 
 """
 Scenario: We are to build a connect 4 game that runs in the terminal
 """
-currentplayer = "O"
-print()
+
 def drawBoard(board):
     for row in board:
         for space in row:
-            print(f"|{space}|", end= ' ')
+            print(f"|{space}|", end = '')
         print()
     """
     This function draws the board in  a nice, clean manner into the terminal.
@@ -29,6 +28,7 @@ def switchPlayer(player):
         return "X"
     elif player == "X":
         return "O"
+
     """
     Switches player from X to O or O to X
     
@@ -41,13 +41,13 @@ def switchPlayer(player):
 
     
 def dropPiece(board,player,column):
-    for row in range(len(board)-1,-1,-1):
+    for row in range(len(board)-1, -1, -1):
         try:
             if board[row][column] == 0:
                 board[row][column] = player
                 return True
         except Exception as e:
-            print(f"An error has occurred. {e}")
+            print(f"An error has occurred: {e}")
     """ 
     Drops piece in specified column
     
@@ -74,32 +74,39 @@ def checkWinner(board,player):
         (BOOL): True if win False if not win 
     """
     #Check Horizontal Win
-    for row in range(3,len(board)):
-            for space in range(0,len(board[row])):
-                if board[row][space] == player:
-                    if board [row][space+1] == player and board[row][space+2] == player and board[row][space+3] == player:
-                        print(f"{player} wins!")
-                        return True
-     
+    for row in range(3, len(board)):
+        for space in range(0, len(board[row])):
+            if board[row][space] == player:
+                if board [row][space+1] == player and board[row][space+2] == player and board[row][space+3] == player:
+                    print(f"{player} wins!")
+                    return True
     #Check Vertical Win
     for row in range(len(board)-3):
         for col in range(len(board[row])):
             if board[row][col] == player:
-                if board [row+1][col] == player and board[row+2][col] == player and board[row+3][col] == player:
+                if board[row+1][col] == player and board[row+2][col] == player and board[row+3][col] == player:
                     print(f"{player} wins!")
                     return True
-
     #Check Left Diagonal win
- 
-    
+    for row in range(len(board)):
+        for space in range(len(board[row])-3):
+            if board[row][space] == player:
+                if board [row-1][space+1] == player and board[row-2][space+2] == player and board[row-3][space+3] == player:
+                    print(f"{player} wins!")
+                    return True
     #Check Right Diagonal Win
-
-    
+    for row in range(len(board)-3):
+        for space in range(len(board[row])):
+            if board[row][space] == player:
+                if board [row+1][space+1] == player and board[row+2][space+2] == player and board[row+3][space+3] == player:
+                    print(f"{player} wins!")
+                    return True
+    return False
 
 def main():
     ROWS = 6
     COLUMNS = 7
-    board = [ 
+    BOARD = [ 
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
@@ -112,7 +119,17 @@ def main():
     #what switch player does is checks to see if current player = x
     #if it is, return O
     #if its not, return X 
-    CURRENT_PLAYER = switchPlayer()
+    while checkWinner(BOARD, CURRENT_PLAYER) == False:
+        print()
+        drawBoard(BOARD)
+        CURRENT_PLAYER = switchPlayer(CURRENT_PLAYER)
+        check1 = False
+        while check1 == False:
+            col = int(input("Enter the column you would like to drop your piece into (1-7): ").strip()) -1
+            if dropPiece(BOARD, CURRENT_PLAYER, col) == True:
+                check1 = True
+                break
+            
     
 if __name__ == "__main__":
     main()
